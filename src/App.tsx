7be0776db21a027a9b2b303677e0533bf0ca1734
Ali-Abdulla Al-Sayed <Ali-Abdulla@alsayed.com>
2024-06-22 21:33:44 +0300

    Update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
+import Home from './views/Home';
 import ChooseRole from './views/Choose-role';
 import AuthoriseDiia from './views/Authorise-diia';
 
 function App() {
-  return (
-    <Router>
-        <Routes>
-          <Route path="choose-role" element={<ChooseRole/>}/>
-          <Route path="authorise-diia" element={<AuthoriseDiia/>}/>
-        </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="choose-role" element={<ChooseRole/>}/>
+                <Route path="authorise-diia" element={<AuthoriseDiia/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
