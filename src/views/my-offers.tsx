9c3b8fc67e417b0969663aa71ac4c795c98722d1
Oleksii Andriushchenko <oleksii.andriushchenko@uptech.team>
2024-06-22 22:49:09 +0300

    feat: add labels

--- src/views/my-offers.tsx
+++ src/views/my-offers.tsx
@@ -1,10 +1,23 @@
+import { HStack } from '../components/HStack';
 import Header from '../components/header';
+import { Label16Bold, Label30 } from '../components/labels';
 
 const MyOffers = () => {
     return (
         <div>
             <Header />
-            <p>My offers</p>
+            <div style={{paddingTop: '3ch', paddingLeft: '15ch', paddingRight: '15ch', paddingBottom: '3ch'}}>
+            <HStack>
+                <Label30 text='Мої оферти'/>
+                <div></div>
+                <button>
+                <Label16Bold text='Створити оферту'/>
+                </button>
+                <button>
+                <Label16Bold text='Додати оферту'/>
+                </button>
+            </HStack>
+            </div>
         </div>
     )
 }
