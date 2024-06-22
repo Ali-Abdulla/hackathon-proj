441adf9a7b1faae886d5bab0182bc79d0191d4a2
Ali-Abdulla Al-Sayed <Ali-Abdulla@alsayed.com>
2024-06-23 02:41:53 +0300

    Update code

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,14 +5,14 @@ const Header = () => {
     return (
         <header className="header-container" style={{height: '64px'}}>
             <a className="header-item">
-                <img src="logo.svg" alt="logo"/>
+                <img src="../logo.svg" alt="logo"/>
             </a>
             <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
                 <a className="header-item">
                     <Label16 text={"ФОП Буданов"}/>
                 </a>
                 <a className="header-item">
-                    <img src="face.svg" alt="face"/>
+                    <img src="../face.svg" alt="face"/>
                 </a>
             </div>
         </header>
