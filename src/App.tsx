dcd1da68ac4dd54b09ddc54f782918b2ec9c76b8
Ali-Abdulla Al-Sayed <Ali-Abdulla@alsayed.com>
2024-06-22 20:15:11 +0300

    Update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Card, Container, FlexColumn, Header } from './components/LargeButton';
+import {Card, Container, FlexColumn} from './components/LargeButton';
+import Header from './components/header';
 
 function App() {
-  return (
-    <div className="app-background">
-      <Container>
-      <Header />
-      <FlexColumn>
-        <Card text="Я надаю послугу" />
-        <Card text="Я отримую послугу" />
-      </FlexColumn>
-    </Container>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <div className="app-background">
+                <Container>
+                    <FlexColumn>
+                        <Card text="Я надаю послугу"/>
+                        <Card text="Я отримую послугу"/>
+                    </FlexColumn>
+                </Container>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/header.tsx
@@ -0,0 +1,20 @@
+const Header = () => {
+    return (
+        <header className="header-container">
+            <a className="header-item">Hackathon Proj</a>
+            <nav>
+                <a className="header-item"
+                   href="#"
+                >Home</a>
+                <a className="header-item"
+                   href="#"
+                >About</a>
+                <a className="header-item"
+                   href="#"
+                >Contact</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
